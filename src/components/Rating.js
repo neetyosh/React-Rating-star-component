d56e92464e4e34ad90652ea91e0c0179d806dd98
Nitesh <pathak.nitsh12@gmail.com>
2021-12-19 17:07:14 +0545

    Initial Commit

---
+++ src/components/Rating.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const Rating = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              size={50}
+              color={ratingValue <= (hover || rating) ? "#f39c12" : "#bdc3c7"}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Rating;
